e0e02c8a0702adf793a2c59d653201a2fc9f5000
santhoshguggilam <santhoshguggilam2007@gmail.com>
2026-04-24 23:36:54 +0530

    Level-1-question-3

---
+++ LargestOfThree.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// Class to check which number is the largest
+class LargestOfThree {
+    public static void main(String[] args) {
+
+        // Create Scanner object for input
+        Scanner input = new Scanner(System.in);
+
+        // Take inputs
+        System.out.print("Enter first number: ");
+        int number1 = input.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = input.nextInt();
+
+        System.out.print("Enter third number: ");
+        int number3 = input.nextInt();
+
+        // Check which is largest
+        boolean isFirstLargest = (number1 > number2) && (number1 > number3);
+        boolean isSecondLargest = (number2 > number1) && (number2 > number3);
+        boolean isThirdLargest = (number3 > number1) && (number3 > number2);
+
+        // Display results
+        System.out.println("Is the first number the largest? " + isFirstLargest);
+        System.out.println("Is the second number the largest? " + isSecondLargest);
+        System.out.println("Is the third number the largest? " + isThirdLargest);
+    }
+}
